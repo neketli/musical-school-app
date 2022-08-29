731084097a6aa85809b935be614f22991b07fe1d
neketli <neketli.cspl@gmail.com>
2022-08-29 22:06:48 +0300

    Added auth form

---
+++ frontend/src/components/BaseInput.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="flex flex-col gap-5">
+    <div v-if="label" class="w-full text-gray-400">
+      {{ label }}
+    </div>
+    <input
+      v-model="value"
+      :type="type"
+      :placeholder="placeholder"
+      class="w-full py-3 px-5 rounded-md border border-gray-300 hover:shadow-sm focus:shadow-md active:shadow-md bg-white text-base text-body-color placeholder-gray-300 outline-none focus:border-gray-500 hover:border-gray-400 transition-all"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    modelValue: {
+      type: [String, Number],
+      required: true,
+    },
+    label: {
+      type: [String],
+      default: "",
+    },
+    type: {
+      type: [String],
+      default: "text",
+    },
+    placeholder: {
+      type: [String],
+      default: "",
+    },
+  },
+  emits: ["update:modelValue"],
+  computed: {
+    value: {
+      get() {
+        return this.modelValue;
+      },
+      set(value) {
+        this.$emit("update:modelValue", value);
+      },
+    },
+  },
+};
+</script>
---
+++ frontend/src/views/AuthView.vue
@@ -0,0 +1,45 @@
+<template>
+  <section class="bg-slate-100 flex justify-center items-center h-[100%]">
+    <div class="container h-full sm:h-auto transition-all">
+      <div
+        class="flex flex-col justify-center items-center mx-auto text-center bg-white shadow-sm hover:shadow-md rounded-lg overflow-hidden py-16 px-10 gap-10 sm:px-12 md:px-[60px] h-full w-full sm:h-auto sm:max-w-[525px] transition-all"
+      >
+        <div class="text-center inline-block max-w-[160px] mx-auto">
+          Музыкальная школа
+        </div>
+        <form class="flex flex-col gap-6">
+          <BaseInput v-model="login" placeholder="Логин" />
+          <BaseInput v-model="password" type="password" placeholder="Пароль" />
+          <BaseButton class="max-w-xs mx-[auto]" @click="auth">
+            Войти
+          </BaseButton>
+        </form>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import BaseInput from "@/components/BaseInput.vue";
+import BaseButton from "@/components/BaseButton.vue";
+
+export default {
+  components: {
+    BaseInput,
+    BaseButton,
+  },
+  data() {
+    return {
+      login: "",
+      password: "",
+    };
+  },
+  methods: {
+    auth() {
+      console.log(this.login, this.password);
+    },
+  },
+};
+</script>
+
+<style lang="scss"></style>

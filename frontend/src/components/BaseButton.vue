c6db13050476a5f89924aaac01e68061adeeccd6
neketli <neketli.cspl@gmail.com>
2022-08-29 22:06:48 +0300

    Added auth form

---
+++ frontend/src/components/BaseButton.vue
@@ -0,0 +1,32 @@
+<template>
+  <button
+    @click.prevent="onClick"
+    class="rounded-md border border-gray-300 py-3 px-5 transition-all"
+    :class="{
+      'bg-gray-50 text-gray-600 hover:border-gray-400 hover:bg-gray-100 hover:shadow-sm active:border-slate-400 active:bg-slate-200':
+        !isDisabled,
+      'bg-gray-300 text-gray-400 cursor-default': isDisabled,
+    }"
+  >
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    isDisabled: {
+      type: [Boolean],
+      default: false,
+    },
+  },
+  emits: { click: null },
+  methods: {
+    onClick() {
+      if (!this.isDisabled) {
+        this.$emit("click");
+      }
+    },
+  },
+};
+</script>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,13 +1,13 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "@/views/HomeView.vue";
+import AuthView from "@/views/AuthView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
-      name: "home",
-      component: HomeView,
+      name: "auth",
+      component: AuthView,
     },
   ],
 });

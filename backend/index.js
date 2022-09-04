65f8788743f1846041d8846703d892f847d895ef
neketli <neketli.cspl@gmail.com>
2022-09-04 17:06:26 +0300

    Added cors

--- backend/index.js
+++ backend/index.js
@@ -1,46 +1,47 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+require("dotenv").config();
 
 // Routes
-const departamentRouter = require('./routes/departaments.routes')
-const subjectsRouter = require('./routes/subjects.routes')
-const specialityRouter = require('./routes/speciality.routes')
-const groupsRouter = require('./routes/groups.routes')
-const studentsRouter = require('./routes/students.routes')
-const teachersRouter = require('./routes/teachers.routes')
-const journalsRouter = require('./routes/journals.routes')
-const classroomsRouter = require('./routes/classrooms.routes')
-const plansRouter = require('./routes/plans.routes')
-const usersRouter = require('./routes/users.routes')
-const studentsGroupsRouter = require('./routes/students_groups.routes')
-const subjectsPlansRouter = require('./routes/subjects_plans.routes')
-const subjectsTeachersRouter = require('./routes/subjects_teachers.routes')
+const departamentRouter = require("./routes/departaments.routes");
+const subjectsRouter = require("./routes/subjects.routes");
+const specialityRouter = require("./routes/speciality.routes");
+const groupsRouter = require("./routes/groups.routes");
+const studentsRouter = require("./routes/students.routes");
+const teachersRouter = require("./routes/teachers.routes");
+const journalsRouter = require("./routes/journals.routes");
+const classroomsRouter = require("./routes/classrooms.routes");
+const plansRouter = require("./routes/plans.routes");
+const usersRouter = require("./routes/users.routes");
+const studentsGroupsRouter = require("./routes/students_groups.routes");
+const subjectsPlansRouter = require("./routes/subjects_plans.routes");
+const subjectsTeachersRouter = require("./routes/subjects_teachers.routes");
 
-const authRouter = require('./routes/auth.routes');
+const authRouter = require("./routes/auth.routes");
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8080;
 
-const app = express()
+const app = express();
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+app.use(cors());
 
-app.use('/api', departamentRouter)
-app.use('/api', subjectsRouter)
-app.use('/api', specialityRouter)
-app.use('/api', groupsRouter)
-app.use('/api', studentsRouter)
-app.use('/api', teachersRouter)
-app.use('/api', journalsRouter)
-app.use('/api', classroomsRouter)
-app.use('/api', plansRouter)
-app.use('/api', usersRouter)
-app.use('/api', studentsGroupsRouter)
-app.use('/api', subjectsPlansRouter)
-app.use('/api', subjectsTeachersRouter)
+app.use("/api", departamentRouter);
+app.use("/api", subjectsRouter);
+app.use("/api", specialityRouter);
+app.use("/api", groupsRouter);
+app.use("/api", studentsRouter);
+app.use("/api", teachersRouter);
+app.use("/api", journalsRouter);
+app.use("/api", classroomsRouter);
+app.use("/api", plansRouter);
+app.use("/api", usersRouter);
+app.use("/api", studentsGroupsRouter);
+app.use("/api", subjectsPlansRouter);
+app.use("/api", subjectsTeachersRouter);
 
-app.use('/api', authRouter)
-
+app.use("/api", authRouter);
 
 // eslint-disable-next-line no-console
-app.listen(PORT, () => console.log(`server started on port: ${ PORT }`))
+app.listen(PORT, () => console.log(`server started on port: ${PORT}`));

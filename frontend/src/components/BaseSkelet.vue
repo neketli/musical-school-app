db8e55ace8f6b1ff9844921c25c388ae2e9863d6
n.li <n.li@ddos-guard.net>
2022-09-21 08:41:46 +0300

    Update skeleton

--- frontend/src/components/BaseSkelet.vue
+++ frontend/src/components/BaseSkelet.vue
@@ -1,50 +1,66 @@
 <template>
-  <div
-    class="ui-skeleton"
-    :style="{ height: `${size}px` }"
-  >
-    <div class="ui-skeleton__loader" />
+  <div class="skeleton" :style="{ height: `${size}px` }">
+    <div class="skeleton__loader" />
   </div>
 </template>
-  
-  <script>
-    export default {
-      props: {
-        size: {
-          type: Number,
-          required: true
-        }
-      }
+
+<script>
+export default {
+  props: {
+    size: {
+      type: Number,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+$base-color: rgba(
+  $color: #b4bbbf,
+  $alpha: 0.35,
+);
+$shine-color: #f0f9ff;
+$animation-duration: 2s;
+
+@mixin background-gradient {
+  background-image: linear-gradient(
+    90deg,
+    $base-color 0px,
+    $shine-color 45px,
+    $base-color 90px
+  );
+  background-size: 200%;
+}
+
+.skeleton {
+  display: flex;
+  width: 100%;
+  height: 100%;
+  border-radius: 10px;
+
+  &__loader {
+    border-radius: 10px;
+    width: 100%;
+    height: 100%;
+
+    box-shadow: 0 2px 5px rgba($color: #000000, $alpha: 0.23);
+    animation: shine $animation-duration infinite;
+    @include background-gradient;
+  }
+
+  @keyframes shine {
+    0% {
+      background-position: -10vw;
     }
-  </script>
-  
-  <style lang="scss">
-    .ui-skeleton {
-      display: flex;
-      background-color: rgba(169, 169, 169, 0.7);
-      width: 100%;
-      height: 100%;
-      border-radius: 10px;
-  
-      &__loader {
-        border-radius: 10px;
-        width: 100%;
-        height: 100%;
-        background: linear-gradient(
-          110deg,
-          rgba(169, 169, 169, 0.7) 8%,
-          rgba(243, 245, 246, 1) 12%,
-          rgba(169, 169, 169, 0.7) 18%
-        );
-        background-size: 200% 100%;
-        animation: 2.5s shine linear infinite;
-      }
-  
-      @keyframes shine {
-        to {
-          background-position-x: -200%;
-        }
-      }
+
+    60% {
+      background-position: 35vw;
     }
-  </style>
-  
+
+    100% {
+      background-position: 100vw;
+    }
+  }
+}
+</style>

c42e50ef1df94f97cda650362e15a7bcc4b1dfae
n.li <n.li@ddos-guard.net>
2022-09-21 11:20:30 +0300

    Refactoring dump errors and views

--- frontend/src/components/BaseFileInput.vue
+++ frontend/src/components/BaseFileInput.vue
@@ -34,13 +34,7 @@
           d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
         />
       </svg>
-      <div class="mb-2 text-sm text-gray-500">
-        Перенесите файл резервной копии в эту область или
-        <span class="font-bold">нажмите</span> чтобы загрузить
-      </div>
-      <div class="mb-2 text-xs text-gray-500">
-        Файл должен быть в формате .tar
-      </div>
+      <slot name="description" />
     </label>
   </div>
   <div

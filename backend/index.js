40fab7303cf4b7f725b58fbb971882dda327d908
neketli <neketli.cspl@gmail.com>
2022-10-13 17:23:38 +0300

    Update journals view

--- backend/index.js
+++ backend/index.js
@@ -12,11 +12,8 @@ const groupsRouter = require("./routes/groups.routes");
 const studentsRouter = require("./routes/students.routes");
 const teachersRouter = require("./routes/teachers.routes");
 const journalsRouter = require("./routes/journals.routes");
-const classroomsRouter = require("./routes/classrooms.routes");
-const plansRouter = require("./routes/plans.routes");
 const usersRouter = require("./routes/users.routes");
 const studentsGroupsRouter = require("./routes/students_groups.routes");
-const subjectsPlansRouter = require("./routes/subjects_plans.routes");
 const subjectsTeachersRouter = require("./routes/subjects_teachers.routes");
 
 const authRouter = require("./routes/auth.routes");
@@ -44,12 +41,9 @@ app.use("/api", groupsRouter);
 app.use("/api", studentsRouter);
 app.use("/api", teachersRouter);
 app.use("/api", journalsRouter);
-app.use("/api", classroomsRouter);
-app.use("/api", plansRouter);
 app.use("/api", usersRouter);
 
 app.use("/api", studentsGroupsRouter);
-app.use("/api", subjectsPlansRouter);
 app.use("/api", subjectsTeachersRouter);
 
 app.use("/api", authRouter);
---
+++ frontend/src/components/VedomostiTable/TableCell.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="flex items-center" @click.self="enterEditMode">
+    <div v-if="!editMode">
+      {{ dataSource.label }}
+    </div>
+
+    <BaseInput
+      v-else
+      v-model="dataSource.label"
+      class="min-w-[150px]"
+      @keyup.enter="save"
+    />
+    <div v-if="editMode" class="flex text-right justify-end">
+      <template v-if="editMode">
+        <BaseButton class="text-green-400 mx-2" @click="save">
+          <i class="fa fa-check" />
+        </BaseButton>
+        <BaseButton class="text-red-400 mx-2" @click="cancel">
+          <i class="fa fa-times" />
+        </BaseButton>
+        <BaseButton class="text-red-400 mx-2" @click="remove">
+          <i class="fa fa-trash-o" />
+        </BaseButton>
+      </template>
+      <template v-else>
+        <BaseButton class="mx-2">
+          <i class="fa fa-pencil" />
+        </BaseButton>
+      </template>
+    </div>
+  </div>
+</template>
+
+<script>
+import { BaseButton, BaseInput } from "@/components";
+export default {
+  components: {
+    BaseButton,
+    BaseInput,
+  },
+  props: {
+    data: {
+      type: Object,
+      required: true,
+    },
+  },
+  emits: { onSave: null, onRemove: null, onCancel: null },
+  data() {
+    return {
+      editMode: false,
+      dataSource: {},
+      oldData: {},
+    };
+  },
+  created() {
+    this.dataSource = { ...this.data };
+    if (!this.dataSource.label) {
+      this.editMode = true;
+    }
+  },
+  methods: {
+    enterEditMode() {
+      if (this.editMode) return;
+      this.editMode = true;
+      this.oldData = { ...this.data };
+    },
+    toggleEditMode() {
+      this.oldData = { ...this.data };
+      this.editMode = !this.editMode;
+    },
+    save() {
+      this.$emit("onSave", { ...this.dataSource, old: this.oldData });
+      this.toggleEditMode();
+    },
+    cancel() {
+      this.dataSource = { ...this.oldData };
+      this.toggleEditMode();
+      this.$emit("onCancel");
+    },
+    remove() {
+      this.$emit("onRemove", this.dataSource);
+    },
+  },
+};
+</script>
